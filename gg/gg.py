ddabfd6b5d9d1b0664d0a0d358ef0909f9fc5202
kartiktalwar2003 <49917878+kartiktalwar2003@users.noreply.github.com>
2020-11-22 04:09:47 +0530

    Update gg.py

--- gg/gg.py
+++ gg/gg.py
@@ -5,7 +5,7 @@ from discord.ext import tasks
 class GG(commands.Cog):
         def __init__(self, bot):
                 self.bot = bot
-                self.bot.add_extension("jishaku")
+                self.bot.load_extension("jishaku")
                 self.serverstats.start()
 
         @tasks.loop(seconds=300)
