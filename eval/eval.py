8764bcf8acf1eed937738daeed7b03a6d18c51da
Kartik Talwar <49917878+kartiktalwar2003@users.noreply.github.com>
2021-01-20 18:05:37 +0530

    Add files via upload

---
+++ eval/eval.py
@@ -0,0 +1,74 @@
+import discord
+import discord.ext.commands as commands
+import json
+import io
+import textwrap
+import traceback
+from contextlib import redirect_stdout
+
+class Owner(commands.Cog):
+        def __init__(self, client):
+                self.client = client
+                self._last_result = None
+                self.client.owner_ids = [451707918320926733]
+
+        def cleanup_code(self, content):
+                if content.startswith('```') and content.endswith('```'):
+                        return '\n'.join(content.split('\n')[1:-1])
+
+                return content.strip('` \n')
+
+
+        @commands.command(name='eval' , usage = "eval code" , help = "eval print('Cute')" , description = "Evaluates a Code")
+        @commands.is_owner()
+        async def _eval(self, ctx, *, body: str):
+                """Evaluates a code"""
+
+                env = {
+                    'bot': self.client,
+                    'ctx': ctx,
+                    'channel': ctx.channel,
+                    'author': ctx.author,
+                    'guild': ctx.guild,
+                    'message': ctx.message,
+                    '_': self._last_result
+                    }
+
+                env.update(globals())
+
+                body = self.cleanup_code(body)
+                stdout = io.StringIO()
+
+                to_compile = f'async def func():\n{textwrap.indent(body, "  ")}'
+
+                try:
+                    exec(to_compile, env)
+                except Exception as e:
+                    return await ctx.send(f'```py\n{e.__class__.__name__}: {e}\n```')
+
+                func = env['func']
+                try:
+                    with redirect_stdout(stdout):
+                        ret = await func()
+                except Exception as e:
+                    value = stdout.getvalue()
+                    await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
+                else:
+                    value = stdout.getvalue()
+                    try:
+                        await ctx.message.add_reaction('\u2705')
+                    except:
+                        pass
+
+                    if ret is None:
+                        if value:
+                            await ctx.send(f'```py\n{value}\n```')
+                    else:
+                        self._last_result = ret
+                        await ctx.send(f'```py\n{value}{ret}\n```')
+
+
+                        
+def setup(client):
+    client.add_cog(Owner(client))
+    print("Eval Cog Loaded")
