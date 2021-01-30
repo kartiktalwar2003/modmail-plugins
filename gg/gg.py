5fa1b67f7b7b7a53c71115cdcfff1e0e54459d26
Kartik Talwar <49917878+kartiktalwar2003@users.noreply.github.com>
2021-01-30 21:09:47 +0530

    Update gg.py

--- gg/gg.py
+++ gg/gg.py
@@ -5,23 +5,6 @@ from discord.ext import tasks
 class GG(commands.Cog):
         def __init__(self, bot):
                 self.bot = bot
-                self.serverstats.start()
-
-        @tasks.loop(seconds=300)
-        async def serverstats(self):
-            try :
-                guild = self.bot.get_guild(477929808022601739)
-                channel = self.bot.get_channel(773068370400641044)
-                mc = guild.member_count
-                await channel.edit(name=f"𝙂𝙖𝙣𝙜 : {mc}")
-
-            except Exception as error :
-                raise error
-                
-                
-        @serverstats.before_loop
-        async def before_serverstats(self):
-            await self.bot.wait_until_ready()
         
         @commands.command(aliases = ["bota","bot"])
         @commands.has_permissions(manage_messages=True)
