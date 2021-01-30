5675bcb3e99329900a992b53fa5fe1ab97d102b0
Kartik Talwar <49917878+kartiktalwar2003@users.noreply.github.com>
2021-01-30 21:16:33 +0530

    Update pog-champ.py

--- gg/pog-champ.py
+++ gg/pog-champ.py
@@ -34,6 +34,31 @@ class PogChamp(commands.Cog):
                         raise error
 
 
+        @commands.command()
+        async def pog(self, ctx):
+                try :
+                        guild = ctx.guild
+                        pog_champ = guild.get_role(786300688866082886)
+                        print(pog_champ)
+
+                        mod_chat = guild.get_channel(746335357079126046)
+                        print(mod_chat)
+
+                        embed = discord.Embed(title = "Pog Champ has left the server!")
+                        embed.description = f"{member.mention} {member} has left the server."
+                        embed.timestamp = datetime.utcnow()
+
+                        if pog_champ in member.roles :
+                                await mod_chat.send(embed = embed)
+
+                        else :
+                                pass
+
+
+                except Exception as error :
+                        raise error
+
+
 def setup(bot):
     bot.add_cog(PogChamp(bot))
     print("PogChamp's Cog Loaded")
