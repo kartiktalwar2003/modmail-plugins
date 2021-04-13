10e1bbf0eedc044f8d6b65c7b86ed1f9d6321949
Kartik Talwar <49917878+kartiktalwar2003@users.noreply.github.com>
2021-04-14 02:55:34 +0530

    Update activityroles.py

--- activityroles/activityroles.py
+++ activityroles/activityroles.py
@@ -25,7 +25,7 @@ class activityroles(commands.Cog):
                                                         if len(activity.type) >= 2 and activity.type == discord.ActivityType.playing and member.bot is False :
                                                                 await member.add_roles(playing)
 
-                                                        elif len(activity.type) != 1 and activity.type != discord.ActivityType.playing and playing in member.roles and member.bot is False :
+                                                        elif len(activity.type) == 1 and activity.type != discord.ActivityType.playing and playing in member.roles and member.bot is False :
                                                                 await member.remove_roles(playing)
 
                                         if member.activity is None :
@@ -70,7 +70,7 @@ class activityroles(commands.Cog):
                                                                         await member.remove_roles(coding)
                                                                         
 
-                                                        elif len(activity.type) != 1 and activity.type != discord.ActivityType.playing and coding in member.roles and member.bot is False :
+                                                        elif len(activity.type) == 1 and activity.type != discord.ActivityType.playing and coding in member.roles and member.bot is False :
                                                                 await member.remove_roles(coding)
 
 
@@ -111,7 +111,7 @@ class activityroles(commands.Cog):
                                                         if len(activity.type) >= 2 and activity.type == discord.ActivityType.listening and member.bot is False :
                                                                 await member.add_roles(spotify)
 
-                                                        elif len(activity.type) != 1 and activity.type != discord.ActivityType.listening and spotify in member.roles and member.bot is False :
+                                                        elif len(activity.type) == 1 and activity.type != discord.ActivityType.listening and spotify in member.roles and member.bot is False :
                                                                 await member.remove_roles(spotify)
 
 
@@ -151,7 +151,7 @@ class activityroles(commands.Cog):
                                                         if len(activity.type) >= 2 and activity.type == discord.ActivityType.streaming and member.bot is False :
                                                                 await member.add_roles(streaming)
 
-                                                        elif len(activity.type) != 1 and activity.type != discord.ActivityType.streaming and streaming in member.roles and member.bot is False :
+                                                        elif len(activity.type) == 1 and activity.type != discord.ActivityType.streaming and streaming in member.roles and member.bot is False :
                                                                 await member.remove_roles(streaming)
 
                                         if member.activity is None :
