d2d3eec109ba1ea34d4275f3629e19278e1440f7
Kartik Talwar <49917878+kartiktalwar2003@users.noreply.github.com>
2021-11-01 01:24:25 +0530

    Update gg.py

--- gg/gg.py
+++ gg/gg.py
@@ -34,6 +34,7 @@ class GG(commands.Cog):
                                 try :
                                     await message.author.remove_roles(nnn_role)
                                     await message.add_reaction("\U0000274e")
+				    await message.reply("Congrats comrade, you are a warrior. Gajju welcomes you to the NNN gang. Remember, no jerking off for the whole November, if you do you're a weakling.")
                                 except :
                                     pass
 
