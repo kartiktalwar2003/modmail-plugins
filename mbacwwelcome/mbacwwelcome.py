9e6312d6067beff5ee42b18c11ad56cb1310905c
Kartik Talwar <49917878+kartiktalwar2003@users.noreply.github.com>
2022-01-07 19:40:34 +0530

    Update mbacwwelcome.py

--- mbacwwelcome/mbacwwelcome.py
+++ mbacwwelcome/mbacwwelcome.py
@@ -10,36 +10,40 @@ class MBACWWelcome(commands.Cog):
 
         @commands.Cog.listener()
         async def on_member_join(self, member):
-            channel = discord.utils.get(member.guild.channels, id=927465465868075028)
-            img_url = member.avatar_url_as(format="png")
-            async with aiohttp.ClientSession() as session:
-                async with session.get(f"{img_url}") as resp:
-                    if resp.status != 200:
-                        return None
-                    data = BytesIO(await resp.read())
-                    avatar = Image.open(data)
-                    avatar = avatar.convert('RGBA').resize((210,210))
-                    mask = Image.new('L', avatar.size, 0)
-                    draw = ImageDraw.Draw(mask)
-                    draw.ellipse((0, 0) + avatar.size, fill=255)
-                    output = ImageOps.fit(avatar, mask.size, centering=(0.5, 0.5))
-                    output.putalpha(mask)
-                    base = Image.open("/app/plugins/kartiktalwar2003/modmail-plugins/mbacwwelcome-main/Welcome.png")
-                    base.paste(output,(408, 50), output)
+            if str(member.guild.id) == "927437635490807820":
+                channel = discord.utils.get(member.guild.channels, id=927465465868075028)
+                img_url = member.avatar_url_as(format="png")
+                async with aiohttp.ClientSession() as session:
+                    async with session.get(f"{img_url}") as resp:
+                        if resp.status != 200:
+                            return None
+                        data = BytesIO(await resp.read())
+                        avatar = Image.open(data)
+                        avatar = avatar.convert('RGBA').resize((210,210))
+                        mask = Image.new('L', avatar.size, 0)
+                        draw = ImageDraw.Draw(mask)
+                        draw.ellipse((0, 0) + avatar.size, fill=255)
+                        output = ImageOps.fit(avatar, mask.size, centering=(0.5, 0.5))
+                        output.putalpha(mask)
+                        base = Image.open("/app/plugins/kartiktalwar2003/modmail-plugins/mbacwwelcome-main/Welcome.png")
+                        base.paste(output,(408, 50), output)
 
-                    myFont = ImageFont.truetype("/app/plugins/kartiktalwar2003/modmail-plugins/mbacwwelcome-main/welcome.ttf", 80)
-                    W, H = (1024,500)
-                    welcome = f"WELCOME {member}"
-                    msg = f"Now we've {member.guild.member_count} members"
-                    draw = ImageDraw.Draw(base)
-                    w, h = draw.textsize(welcome, font=myFont)
-                    draw.text(((W-w)/2, 300), welcome, fill="hotpink", font=myFont)
-                    w, h = draw.textsize(msg, font=myFont)
-                    draw.text(((W-w)/2, 390), msg, fill="black", font=myFont)
-                    base.save("card.png")
-                
-                file = discord.File("card.png", filename="card.png")
-                await channel.send(content = f"Welcome {member.mention} have a Kadak Masaledar Chai while you're here!" , file = file)
+                        myFont = ImageFont.truetype("/app/plugins/kartiktalwar2003/modmail-plugins/mbacwwelcome-main/welcome.ttf", 80)
+                        W, H = (1024,500)
+                        welcome = f"WELCOME {member}"
+                        msg = f"Now we've {member.guild.member_count} members"
+                        draw = ImageDraw.Draw(base)
+                        w, h = draw.textsize(welcome, font=myFont)
+                        draw.text(((W-w)/2, 300), welcome, fill="hotpink", font=myFont)
+                        w, h = draw.textsize(msg, font=myFont)
+                        draw.text(((W-w)/2, 390), msg, fill="black", font=myFont)
+                        base.save("card.png")
+                    
+                    file = discord.File("card.png", filename="card.png")
+                    await channel.send(file = file)
+            
+            else :
+                return
 
                         
 def setup(bot):
