97ea2b76d686c3b42de564eddc723cf93b997166
Kartik Talwar <49917878+kartiktalwar2003@users.noreply.github.com>
2022-03-31 02:51:15 +0530

    Update tts.py

--- tts/tts.py
+++ tts/tts.py
@@ -29,7 +29,7 @@ class TTS(commands.Cog):
                         await author_channel.channel.connect()
                         print("Connected")
                     except Exception as e:
-                        raise e
+                        print(e)
                     await ctx.reply(f"Joined {author_channel.channel.mention}", mention_author=False)
                     
                 elif connect_perms or speak_perms is False:
